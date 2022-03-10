ed979f09874a9b8739493edc7c58ba23bb8a800c
bxvu <bxvu@ucsc.edu>
2022-03-09 21:17:08 -0800

    worked on part 2

--- lab4/lab4_part2.s
+++ lab4/lab4_part2.s
@@ -17,6 +17,21 @@
 draw_pixel:
 
     # Your code goes here
-    
+    li t0, 0xc00
+    li t1, 0
+    li t2, 0
+
+    beq t1, a1, 2f
+1:
+    addi t0, t0, 32
+    addi t1, t1, 1
+    blt t1, a1, 1b
+
+2:
+    addi t0, t0, 1
+    addi t2, t2, 1
+    blt t2, a2, 2b
+
+    sb a0, 0(t0)
     jr ra
     
