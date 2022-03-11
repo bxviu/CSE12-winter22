1b01debec031aad357d62b1e154d9ca7a66519e8
bxvu <bxvu@ucsc.edu>
2022-03-11 13:09:30 -0800

    finalized all parts

--- lab4/lab4_part2.s
+++ lab4/lab4_part2.s
@@ -18,20 +18,23 @@ draw_pixel:
 
     # Your code goes here
     li t0, 0xc00
-    li t1, 0
-    li t2, 0
+    li t2, 0xfff
+    
+    slli t1, a1, 5
+    add t1, t0, a1
+    blt t1, t0, 1f
+    bgt t1, t2, 1f
 
-    beq t1, a1, 2f
-1:
-    addi t0, t0, 32
-    addi t1, t1, 1
-    blt t1, a1, 1b
+    add t1, t0, a2
+    blt t1, t0, 1f
+    bgt t1, t2, 1f
 
-2:
-    addi t0, t0, 1
-    addi t2, t2, 1
-    blt t2, a2, 2b
+    slli t1, a1, 5
+    add t0, t0, t1
 
+    add t0, t0, a2
     sb a0, 0(t0)
+
+1:
     jr ra
     
